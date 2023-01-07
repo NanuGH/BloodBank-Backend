9a58982245c92ecbc5ebee2f31997b23399b0bbc
NanuGH <66637776+NanuGH@users.noreply.github.com>
2023-01-07 00:36:39 -0100

    another one

--- src/test/java/cv/ady/defaultprojectspring/DefaultprojectspringApplicationTests.java
+++ src/test/java/cv/ady/defaultprojectspring/DefaultprojectspringApplicationTests.java
@@ -1,4 +1,4 @@
-package cv.ady.defaultprojectspring;
+/* package cv.ady.defaultprojectspring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class DefaultprojectspringApplicationTests {
 	}
 
 }
+ */
